2cf2bea91d7d9cd6889ca6ec1d1104c6937eec2b
Katie Farquhar <katie.farquhar@gmail.com>
2019-08-26 07:13:19 -0700

    Ask user for name, then compliment user

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,6 +1,17 @@
+import random
+
+def random_compliment():
+    complimentList = ["kind","lovely","awesome","amazing","one cool cat"]
+    randInt = random.randint(0, len(complimentList)-1) 
+    return complimentList[randInt]
+
 def main():
-    msg = "Hello World!"
+    msg = "Hello World! What's your name?"
     print(msg)
+    name = input()
+    compliment = random_compliment()
+    msg2 = "Nice to meet you, " + name + "! You are " + compliment + "!"
+    print(msg2)
 
 if __name__ == '__main__':
     main()
